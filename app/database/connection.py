6d42fb11f02edcb96fbeea215d7374c3710650a1
Dhruv Sharma <dhruv.sharma@testingxperts.com>
2026-03-10 15:27:15 +0530

    render deployment

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,27 +1,53 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-import time
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+# import time
 
 # DATABASE_URL = "mysql+pymysql://root:Password@localhost:3306/ecommerce"
-DATABASE_URL = "mysql+pymysql://root:Password@mysql:3306/ecommerce"
+# # DATABASE_URL = "mysql+pymysql://root:Password@mysql:3306/ecommerce"
 
-engine = None
-for i in range(10):
-    try:
-        engine = create_engine(DATABASE_URL, echo=False)
-        conn = engine.connect()
-        conn.close()
-        print("Connected to MySQL")
-        break
-    except Exception as e:
-        print("Waiting for MySQL...")
-        time.sleep(3)
-if engine is None:
-    raise Exception("Could not connect to MySQL")
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# engine = None
+# for i in range(10):
+#     try:
+#         engine = create_engine(DATABASE_URL, echo=False)
+#         conn = engine.connect()
+#         conn.close()
+#         print("Connected to MySQL")
+#         break
+#     except Exception as e:
+#         print("Waiting for MySQL...")
+#         time.sleep(3)
+# if engine is None:
+#     raise Exception("Could not connect to MySQL")
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base = declarative_base()
+
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://root:Password@localhost:3306/ecommerce"
+)
+
+engine = create_engine(DATABASE_URL, echo=False)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
