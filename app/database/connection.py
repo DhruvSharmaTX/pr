6975e287f6b4c179545b074dfa16bc21a6c6fdb5
Dhruv Sharma <dhruv.sharma@testingxperts.com>
2026-03-11 16:05:55 +0530

    prepare for render deployment

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,27 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import time
+import os
 
-DATABASE_URL = "mysql+pymysql://root:Password@localhost:3306/ecommerce"
-# DATABASE_URL = "mysql+pymysql://root:Password@mysql:3306/ecommerce"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = None
-for i in range(10):
-    try:
-        engine = create_engine(DATABASE_URL, echo=False)
-        conn = engine.connect()
-        conn.close()
-        print("Connected to MySQL")
-        break
-    except Exception as e:
-        print("Waiting for MySQL...")
-        time.sleep(3)
-if engine is None:
-    raise Exception("Could not connect to MySQL")
+engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
@@ -29,4 +16,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
